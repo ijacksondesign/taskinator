fdadba752a390bbebc2562f75a13c2e6c02a8ab5
Ian Jackson <ijacksondesign@gmail.com>
2021-01-11 22:26:51 -0500

    added form validation and for reset

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,6 +9,12 @@ var taskFormHandler = function(event) {
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     var taskTypeInput = document.querySelector("select[name='task-type']").value
 
+    // check if input values are empty strings
+    if (!taskNameInput || !taskTypeInput) {
+        alert("You need to fill out the task form!");
+        return false;
+    };
+
     // package up data as an object
     var taskDataObj = {
         name: taskNameInput,
@@ -17,6 +23,7 @@ var taskFormHandler = function(event) {
 
     // send it as an argument to createTaskEl
     createTaskEl(taskDataObj);
+    formEl.reset();
 };
 
 var createTaskEl = function(taskDataObj) {
@@ -27,8 +34,6 @@ var createTaskEl = function(taskDataObj) {
     //  create div to hold task info and add to list item
     var taskInfoEl = document.createElement("div");
     taskInfoEl.className = "task-info";
-
-    //  adds HTML content to div
     taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
     listItemEl.appendChild(taskInfoEl);
     
